59c34a8fa2ff1347c03fdfa7e8b4dc27fd30ddce
MenjiShad <jamesdinh14@gmail.com>
2018-05-20 23:30:55 -0400

    Add config for UserRole data model

---
+++ Collab.API/Models/Config/UserRoleConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Collab.API.Models.Config
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.ToTable("UserRoles")
+                .HasKey(ur => new { ur.UserId, ur.RoleId });
+            
+            builder.Ignore(ur => ur.Id);
+                
+            builder.HasOne(ur => ur.User)
+                .WithMany(u => u.Roles)
+                .HasForeignKey(ur => ur.UserId);
+
+            builder.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+        }
+    }
+}
--- Collab.API/Models/UserRole.cs
+++ Collab.API/Models/UserRole.cs
@@ -3,11 +3,9 @@ namespace Collab.API.Models
     public class UserRole : BaseModel
     {
         public int UserId { get; set; }
-
         public virtual User User { get; set; }
 
         public int RoleId { get; set; }
-
         public virtual Role Role { get; set; }
     }
 }
