a38a46c4f794e37a7299a880990ef87b25b86eda
MenjiShad <jamesdinh14@gmail.com>
2018-05-21 18:23:13 -0400

    Add another command to localdeploy script

--- localdeploy.sh
+++ localdeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Shortcut to running both of the docker commands below for a faster build and deploy:
 #   bash localdeploy.sh
+docker swarm init
 docker-compose -f docker-compose.override.dev.yml build
 docker stack deploy -c docker-compose.override.dev.yml collab
