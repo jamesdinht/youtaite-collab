110f69f47f035048acd2838e8844614bc486fb92
MenjiShad <jamesdinh14@gmail.com>
2018-05-24 22:29:12 -0400

    Remove foreign key property from Group entity

--- Collab.API/Models/Group.cs
+++ Collab.API/Models/Group.cs
@@ -3,11 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Collab.API.Models
 {
+    /// <summary>
+    /// Data entity representing a Group, or a collection of Users.
+    /// </summary>
     public class Group : BaseModel
     {
         public string Name { get; set; }
 
-        public int ProjectId { get; set; }
         public virtual ICollection<Project> Projects { get; set; }
 
         public virtual ICollection<GroupMember> Users { get; set; }
