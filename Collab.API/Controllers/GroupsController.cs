5b9eb483fb64f32d89e44326e864e11bab25a21a
MenjiShad <jamesdinh14@gmail.com>
2018-05-27 23:08:38 -0400

    Add route to Groups api

--- Collab.API/Controllers/GroupsController.cs
+++ Collab.API/Controllers/GroupsController.cs
@@ -1,8 +1,10 @@
 using Collab.API.DAL;
 using Collab.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Collab.API.Controllers
 {
+    [Route("api/[controller]")]
     public class GroupsController : AController<Group>
     {
         public GroupsController(IRepository<Group> db) : base(db)
