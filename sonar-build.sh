7536f80b62bc35a50d21e03003321eae56995ce0
MenjiShad <jamesdinh14@gmail.com>
2018-05-29 22:37:19 -0400

    Remove references to old directory

--- sonar-build.sh
+++ sonar-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dotnet tools/sonar/SonarScanner.MSBuild.dll begin /k:youtaite-collab /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io"
+dotnet sonar/SonarScanner.MSBuild.dll begin /k:youtaite-collab /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io"
 dotnet build --no-restore
-dotnet tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
+dotnet sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
--- sonar-install.sh
+++ sonar-install.sh
@@ -4,5 +4,5 @@ sudo apt-get -qq update
 wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip
 unzip -qq sonar.zip -d sonar
 ls -l sonar
-chmod +x tools/sonar/sonar-scanner-3.1.0.1141/bin/sonar-scanner
-chmod +x tools/sonar/SonarScanner.MSBuild.dll
+chmod +x sonar/sonar-scanner-3.1.0.1141/bin/sonar-scanner
+chmod +x sonar/SonarScanner.MSBuild.dll
