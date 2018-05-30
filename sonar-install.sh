9706bf9b6e563a0c4b82801965fac7dfa708b5a7
MenjiShad <jamesdinh14@gmail.com>
2018-05-29 22:35:50 -0400

    Update unzip directory for sonar installation

--- sonar-install.sh
+++ sonar-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get -qq update
 wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip
-unzip -qq sonar.zip -d tools/sonar
-ls -l tools/sonar
+unzip -qq sonar.zip -d sonar
+ls -l sonar
 chmod +x tools/sonar/sonar-scanner-3.1.0.1141/bin/sonar-scanner
 chmod +x tools/sonar/SonarScanner.MSBuild.dll
