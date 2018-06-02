df1e9cb385dd08be39acfe7cf8cb42f07f8b78fe
MenjiShad <jamesdinh14@gmail.com>
2018-06-02 12:05:05 -0400

    Fix test cases

--- Collab.Interface/src/app/app.component.spec.ts
+++ Collab.Interface/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -10,7 +11,8 @@ describe('AppComponent', () => {
         UsersComponent
       ],
       imports: [
-        HttpClientModule
+        HttpClientModule,
+        MatToolbarModule
       ]
     }).compileComponents();
   }));
@@ -24,10 +26,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Youtaite Collab');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render material toolbar', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Youtaite Collab!');
+    expect(compiled.querySelector('mat-toolbar'));
   }));
 });
