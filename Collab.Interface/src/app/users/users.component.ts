59d90b916cc18979e35938471dd6595c504d5b81
MenjiShad <jamesdinh14@gmail.com>
2018-06-02 23:56:41 -0400

    Implement UserService in UserComponent

--- Collab.Interface/src/app/users/users.component.ts
+++ Collab.Interface/src/app/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from './user';
+import { User } from '../models/user';
 import { UserService } from './user.service';
 import { Observable } from 'rxjs';
 
@@ -11,19 +11,76 @@ import { Observable } from 'rxjs';
 export class UsersComponent implements OnInit {
 
   users: User[];
+  selectedUser: User;
+  operationSuccess: boolean;
 
   constructor(private userService: UserService) { }
-
-  getAllUsers() {
-    return this.userService.getAllUsers().subscribe(
-      data => this.users = data,
-      err => console.error(err),
-      () => {}
-    );
-  }
 
   ngOnInit() {
     this.getAllUsers();
   }
 
+  getAllUsers() {
+    return this.userService.getAllUsers().subscribe(
+      response => {
+        this.users = response;
+        this.operationSuccess = true;
+      },
+      errorResponse => this.onError(errorResponse, 'getAllUsers'),
+      () => this.onCompleted('getAllUsers')
+    );
+  }
+
+  getUserById(id: number) {
+    return this.userService.getUserById(id).subscribe(
+      response => {
+        this.selectedUser = response;
+        this.operationSuccess = true;
+      },
+      errorResponse => this.onError(errorResponse, 'getUserById'),
+      () => this.onCompleted('getUserById')
+    );
+  }
+
+  createUser(user: User) {
+    return this.userService.createUser(user).subscribe(
+      response => {
+        this.users.push(response);
+        this.selectedUser = response;
+        this.operationSuccess = true;
+      },
+      errorResponse => this.onError(errorResponse, 'createUser'),
+      () => this.onCompleted('createUser')
+    );
+  }
+
+  updateUser(updatedUser: User) {
+    return this.userService.updateUser(updatedUser).subscribe(
+      response => {
+        this.selectedUser = response;
+        this.operationSuccess = true;
+      },
+      errorResponse => this.onError(errorResponse, 'updateUser'),
+      () => this.onCompleted('updateUser')
+    );
+  }
+
+  deleteUser(userToDelete: User) {
+    return this.userService.deleteUser(userToDelete).subscribe(
+      response => {
+        this.users.findIndex(user => user.id === userToDelete.id);
+      },
+      errorResponse => this.onError(errorResponse, 'deleteUser'),
+      () => this.onCompleted('deleteUser')
+    );
+  }
+
+  onError(errorResponse: any, operation: string) {
+    this.operationSuccess = false;
+    console.error(`${operation} errored with: ${errorResponse}`);
+  }
+
+  onCompleted(operation: string) {
+    console.log(`${operation} completed`);
+  }
 }
