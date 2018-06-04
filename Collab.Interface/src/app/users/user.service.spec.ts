cf8673513a59dadb7e1f2f5e8c808e0a16dc2d6e
MenjiShad <jamesdinh14@gmail.com>
2018-06-03 22:05:39 -0400

    Add tests for GET methods

--- Collab.Interface/src/app/users/user.service.spec.ts
+++ Collab.Interface/src/app/users/user.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { UserService } from './user.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 describe('UserService', () => {
   beforeEach(() => {
@@ -10,12 +11,60 @@ describe('UserService', () => {
         UserService,
       ],
       imports: [
-        HttpClientModule
+        HttpClientTestingModule,
       ]
     });
   });
 
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
+  let service: UserService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(inject([UserService], s => {
+    service = s;
   }));
+
+  beforeEach(inject([HttpTestingController], http => {
+    httpMock = http;
+  }));
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch data from the correct URL', () => {
+
+    service.getAllUsers().subscribe(response => {
+      expect(response).toBeTruthy();
+      expect(response.length).toBe(2);
+    });
+    const request = httpMock.expectOne(environment.usersUrl);
+    expect(request.request.method).toEqual('GET');
+
+    request.flush([
+      { id: 1, nickname: 'James' },
+      { id: 2, nickname: 'Menji' }
+    ]);
+  });
+
+  it('should fetch a single user correctly', () => {
+
+      const id = 1;
+      service.getUserById(id).subscribe(response => {
+        expect(response).toBeTruthy();
+        expect(response.id).toBe(id);
+        expect(response.nickname).toBe('James');
+      });
+
+      const request = httpMock.expectOne(`${environment.usersUrl}/${id}`);
+      expect(request.request.method).toEqual('GET');
+
+      request.flush(
+        { id: 1, nickname: 'James' }
+      );
+    });
+
 });
