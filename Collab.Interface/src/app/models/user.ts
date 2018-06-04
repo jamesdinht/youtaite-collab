298a94eebe396efc142208a9509668adbe55b8de
MenjiShad <jamesdinh14@gmail.com>
2018-06-03 23:44:06 -0400

    Add test to test correct POST

--- Collab.Interface/src/app/models/user.ts
+++ Collab.Interface/src/app/models/user.ts
@@ -1,4 +1,9 @@
 export class User {
     id: number;
     nickname: string;
+
+    constructor(id: number, nickname: string) {
+        this.id = id;
+        this.nickname = nickname;
+    }
 }
--- Collab.Interface/src/app/users/user.service.spec.ts
+++ Collab.Interface/src/app/users/user.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { environment } from 'src/environments/environment';
+import { User } from 'src/app/models/user';
 
 describe('UserService', () => {
   beforeEach(() => {
@@ -53,18 +54,45 @@ describe('UserService', () => {
   it('should fetch a single user correctly', () => {
 
       const id = 1;
+      const nickname = 'James';
+      const fakeUser = new User(id, nickname);
+
       service.getUserById(id).subscribe(response => {
         expect(response).toBeTruthy();
-        expect(response.id).toBe(id);
-        expect(response.nickname).toBe('James');
+        expect(response).toEqual(fakeUser);
       });
 
       const request = httpMock.expectOne(`${environment.usersUrl}/${id}`);
       expect(request.request.method).toEqual('GET');
 
       request.flush(
-        { id: 1, nickname: 'James' }
+        fakeUser
       );
     });
 
+    it('should return an error message on 404', () => {
+      // TODO: Elaborate more. Perhaps, add some kind of error response to service
+      const id = 1;
+      service.getUserById(id).subscribe(response => {
+        expect(response).toBeFalsy();
+      });
+
+      const request = httpMock.expectOne(`${environment.usersUrl}/${id}`);
+      expect(request.request.method).toEqual('GET');
+    });
+
+    it('should add new user correctly', () => {
+
+      const id = 1;
+      const nickname = 'James';
+      const newUser = new User(1, nickname);
+
+      service.createUser(newUser).subscribe(response => {
+        expect(response).toBeTruthy();
+        expect(response).toEqual(newUser);
+      });
+
+      const request = httpMock.expectOne(`${environment.usersUrl}`);
+      expect(request.request.method).toBe('POST');
+    });
 });
