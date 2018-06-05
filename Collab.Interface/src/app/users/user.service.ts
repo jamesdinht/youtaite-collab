9492a72f3b1ce09d1e52226f4d27dd1f8d5c98c1
MenjiShad <jamesdinh14@gmail.com>
2018-06-05 19:28:38 -0400

    Add http header options

--- Collab.Interface/src/app/users/user.service.ts
+++ Collab.Interface/src/app/users/user.service.ts
@@ -12,6 +12,12 @@ export class UserService {
 
   constructor(private httpClient: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
   getAllUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(environment.usersUrl)
       .pipe(
@@ -20,23 +26,25 @@ export class UserService {
   }
 
   getUserById(id: number): Observable<User> {
-    return this.httpClient.get<User>(`${environment.usersUrl}/${id}`)
+    return this.httpClient.get<User>(`${environment.usersUrl}/${id}`, this.httpOptions)
     .pipe(
       catchError(this.handleError<User>(`getUserbyId id=${id}`))
     );
   }
 
   createUser(user: User): Observable<User> {
-    const body = JSON.stringify({ Name: User.name });
-    return this.httpClient.post<User>(environment.usersUrl, body);
+    return this.httpClient.post<User>(environment.usersUrl, user, this.httpOptions);
   }
 
   updateUser(updatedUser: User): Observable<User> {
-    return this.httpClient.put<User>(`${environment.usersUrl}/${updatedUser.id}`, updatedUser);
+    return this.httpClient.put<User>(`${environment.usersUrl}/${updatedUser.id}`, updatedUser, this.httpOptions)
+      .pipe(
+        catchError(this.handleError('updateUser', updatedUser))
+      );
   }
 
   deleteUser(userToBeDeleted: User): Observable<User[]> {
-    return this.httpClient.delete<User[]>(environment.usersUrl + userToBeDeleted.id);
+    return this.httpClient.delete<User[]>(environment.usersUrl + userToBeDeleted.id, this.httpOptions);
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
