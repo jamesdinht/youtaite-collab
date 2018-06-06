620f75c4ece6a05309b10625a4745d60cbf09747
MenjiShad <jamesdinh14@gmail.com>
2018-06-06 18:56:19 -0400

    Change import reference to moved UserService

--- Collab.Interface/src/app/users/users.component.ts
+++ Collab.Interface/src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
-import { UserService } from './user.service';
+import { UserService } from 'src/app/shared/user.service';
 import { Observable } from 'rxjs';
 
 @Component({
