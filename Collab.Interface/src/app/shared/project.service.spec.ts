12384d6754c5b8e6ac6fd2b98f5e12c26c3496e3
MenjiShad <jamesdinh14@gmail.com>
2018-06-08 00:18:29 -0400

    Include HttpClientModule to fix ProjectService test

--- Collab.Interface/src/app/shared/project.service.spec.ts
+++ Collab.Interface/src/app/shared/project.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ProjectService } from './project.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProjectService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProjectService]
+      providers: [ProjectService],
+      imports: [
+        HttpClientTestingModule
+      ]
     });
   });
 
