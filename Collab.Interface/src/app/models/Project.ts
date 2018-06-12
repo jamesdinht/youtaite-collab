3ddc2b22733ce62613adebcf9fc062e0c81adf40
MenjiShad <jamesdinh14@gmail.com>
2018-06-12 00:33:38 -0400

    Make Project inputs optional

--- Collab.Interface/src/app/models/Project.ts
+++ Collab.Interface/src/app/models/Project.ts
@@ -3,7 +3,7 @@ import { BaseModel } from './BaseModel';
 export class Project extends BaseModel {
     name: string;
 
-    constructor(id: number, name: string) {
+    constructor(id?: number, name?: string) {
         super(id);
         this.name = name;
     }
