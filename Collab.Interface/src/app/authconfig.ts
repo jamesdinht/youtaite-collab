0f4a762efb9a5a0dbecb69dfd6182cb3677386c5
MenjiShad <jamesdinh14@gmail.com>
2018-06-16 21:21:27 -0400

    Initial commit - AuthConfig

---
+++ Collab.Interface/src/app/authconfig.ts
@@ -0,0 +1,26 @@
+interface AuthConfig {
+    CLIENT_ID: string;
+    CLIENT_DOMAIN: string;
+    RESPONSETYPE: string;
+    AUDIENCE: string;
+    REDIRECT: string;
+    RETURNTO: string;
+    SCOPE: string;
+}
+
+export class Config {
+
+    AUTH_CONFIG: AuthConfig;
+
+    constructor() {
+        this.AUTH_CONFIG = {
+            CLIENT_ID: 'c6DwkGblBC8vr9lnLFPpJ5a79MeEkeuc',
+            CLIENT_DOMAIN: 'jamesdinht.auth0.com',
+            RESPONSETYPE: 'token id_token',
+            AUDIENCE: 'https://jamesdinht.auth0.com/userinfo',
+            REDIRECT: 'http://localhost:9100/callback',
+            RETURNTO: 'http://localhost:9100/',
+            SCOPE: 'openid'
+        };
+    }
+}
